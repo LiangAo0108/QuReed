87c2b52aac9b6121ff31b0023f6d98864d53611f
LiangAo0108 <liangao0108@163.com>
2024-09-09 18:59:35 +0800

    a MIZ, but error: float() argument must be a string or a real number, not 'GenericQuantumSignal'

--- quasi/devices/extra/mach_zehnder_interferometer.py
+++ quasi/devices/extra/mach_zehnder_interferometer.py
@@ -1,5 +1,5 @@
 """
-Unit Cell
+MZI
 """
 import traceback
 from quasi.devices import (GenericDevice,
@@ -84,11 +84,10 @@ class MachZehnderInterferometer(GenericDevice):
     def __init__(self, name=None, uid=None): # initialize MZI including device name and unique identifier
         super().__init__(name=name, uid=uid)
         self.phase_shift = 0
-        self.qin0 = 0
-        self.qin1 = 0
+        self.qin0 = GenericQuantumSignal()
+        self.qin1 = GenericQuantumSignal()
         self.beam_splitter = MZI_BeamSplitter()
         self.phase_shifter = MZI_PhaseShifter()
-
 
     @wait_input_compute
     def compute_outputs(self,  *args, **kwargs):
@@ -101,18 +100,25 @@ class MachZehnderInterferometer(GenericDevice):
     @log_action
     @schedule_next_event
     def des(self, time=None, *args, **kwargs):
+        signals = kwargs.get("signals", {"qin0": GenericQuantumSignal(),
+            "qin1": GenericQuantumSignal(),
+            "phase_shift": GenericFloatSignal()})
 
-        signals = kwargs.get("signals")
+        #signal = kwargs.get("signals")
 
         # check if input signal is existing
         if "qin0" in signals:
             self.qin0 = kwargs["signals"]["qin0"].contents
         else:
             logger.error("error: qin0 signal is missing or none")
-            self.qin0 = 0
+            self.qin0 = GenericQuantumSignal()
+        if isinstance(self.qin0, GenericQuantumSignal):
+            pass
+        else:
+            raise ValueError("qin0 is not a GenericQuantumSignal")
+
+
         if "qin1" in signals:
-            #in_qin1 = signals["qin1"]
-            #env1 = kwargs["signals"]["qin1"].contents
             self.qin1 = kwargs["signals"]["qin1"].contents
         else:
             logger.error("error: qin1 signal is missing or none")
@@ -126,7 +132,6 @@ class MachZehnderInterferometer(GenericDevice):
             #self.phase_shift = 0
             raise ValueError("phase_shift signal is missing.")
 
-
         # through the first beam splitter, the output named bs_output
         bs_output0, bs_output1 = self.beam_splitter.apply(self.qin0, self.qin1)
 
@@ -138,8 +143,8 @@ class MachZehnderInterferometer(GenericDevice):
 
         # output signal
         self.ports["qout0"].signal = GenericQuantumSignal(qout0)
-        output_signal_0 = GenericQuantumSignal(qout0)  # 输出端口0的信号
-        output_signal_1 = GenericQuantumSignal(qout1)  # 输出端口1的信号
+        output_signal_0 = GenericQuantumSignal(qout0)  # output the signal on port 0
+        output_signal_1 = GenericQuantumSignal(qout1)  # output the signal on port 1
 
         try:
             result = [("qout0", output_signal_0, time), ("qout1", output_signal_1, time)]
